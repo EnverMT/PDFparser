193a6a1fa5cdb3790eb4e009f19d753e3030b738
EnverMT <anvarmtg@gmail.com>
2023-01-16 14:19:07 +0500

    added GetTextPart method

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from pdfminer.high_level import extract_text
 
@@ -26,3 +27,26 @@ class parse():
                 file.write(extract_text(os.path.join(root, f)))
                 file.close()
                 print(f"File parsed: '{filename}'")
+
+    def getTextPart(self, start_text : str, end_text : list[str]) -> str:
+        result = str()
+        body = False
+        start = start_text
+        if len(end_text) == 1:
+            end = end_text[0]
+        else:
+            end = '|'.join(end_text)
+
+        for root, d_names, f_names in os.walk(self.path_result):
+            for f in f_names:
+                with open(os.path.join(root, f), 'r', encoding='utf-8') as file:
+                    for line in file:
+                        if re.search(start, line):
+                            body = True
+                            continue
+                        if re.search(end, line) and body == True:
+                            body = False
+                            break
+                        if body:
+                            result += line
+                    return result
